fbe6f7b4b789d6c6506db3793809813c20bf1525
dlthgml1997 <shfksthgml@naver.com>
2021-01-19 23:28:27 +0900

    solved / 재귀

---
+++ 수학/거듭 제곱.py
@@ -0,0 +1,11 @@
+# https://swexpertacademy.com/main/solvingProblem/solvingProblem.do
+
+def multiple(answer, num, cnt, limit):
+    if cnt == limit:
+        return answer
+    return multiple(answer * num, num, cnt + 1, limit)
+
+for _ in range(10):
+    t = input()
+    a, b = map(int, input().split())
+    print("#"+t+" "+str(multiple(a, a, 1, b)))
