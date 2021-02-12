e35daf175ea73d4507ab55de11ad1f4eb4028969
dlthgml1997 <shfksthgml@naver.com>
2021-02-12 17:13:02 +0900

    ac: boj 5397 키로거 / Deque
    NoSuchElementError 해결 (비어있는 Deque에서 pop => isEmpty)

---
+++ 자료구조-큐/Main_BOJ_5397_키로거.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_5397_키로거 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		int T = stoi(br.readLine());
+		for (int i = 0; i < T; i++) {
+			Deque<Character> pw = new ArrayDeque<Character>();
+			Deque<Character> wait = new ArrayDeque<Character>();
+
+			char[] ch = br.readLine().toCharArray();
+			for (int j = 0; j < ch.length; j++) {
+				char c = ch[j];
+				if (c == '<' || c == '>' || c == '-') {
+					switch (c) {
+					case '<': {
+						if (pw.isEmpty())
+							break;
+						wait.addFirst(pw.removeLast());
+						break;
+					}
+					case '>': {
+						if (wait.isEmpty())
+							break;
+						pw.addLast(wait.removeFirst());
+						break;
+					}
+					case '-':
+//						if(pw.isEmpty()) break;
+						pw.removeLast();
+						break;
+					}
+				} else {
+					pw.addLast(c);
+				}
+			}
+			
+			while (!wait.isEmpty())
+				pw.addLast(wait.removeFirst());
+			while (!pw.isEmpty())
+				bw.append(pw.removeFirst()+"");
+			bw.append("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+	private static int stoi(String input) {
+		return Integer.parseInt(input);
+	}
+}
